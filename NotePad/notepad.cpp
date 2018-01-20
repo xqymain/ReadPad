3005f409c8c91e38566016364304484d0e4ccc85
Xing Qingyu <201330010330@hnu.edu.cn>
2018-01-20 21:46:08 +0800

    Update notepad.cpp

--- NotePad/notepad.cpp
+++ NotePad/notepad.cpp
@@ -1,4 +1,12 @@
-//包含头文件
+/*
+ * Program Name: ReadPad
+ * Author: xqymain
+ * Under MIT protocol
+ * Build Pass by Visual Stdio 2017
+ */
+
+// Include
+//
 #include <Windows.h>
 #include <tchar.h>
 #include <Shlwapi.h>
@@ -6,38 +14,41 @@
 
 #pragma comment(lib,"Shlwapi.lib")
 
-//宏定义
+// Defined
 #define IDC_EDIT 1001
-//全局变量
+
+// Global variables
 HINSTANCE g_hInstance;
-TCHAR g_szFilePath[MAX_PATH];  //打开文件路径
+TCHAR g_szFilePath[MAX_PATH];  // File Path
 TCHAR g_szFileTitle[MAX_PATH];
-//函数声明
+
+// Function declaration
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-//入口函数 WinMain()
+
+// Entrance function
 int WINAPI WinMain(HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine,
 	int nCmdShow)
 {
 	g_hInstance = hInstance;
-	//构建窗口类 
+	// Create WndClass
 	WNDCLASS WndClass;
-	WndClass.cbClsExtra = 0; //类之后的内存空间默认字节数
-	WndClass.cbWndExtra = 0; //窗口实例默认额外的字节数
-	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);     //软件背景 画刷
-	WndClass.hCursor = LoadCursor(NULL, MAKEINTRESOURCE(IDC_ARROW));  //加载光标
-	WndClass.hIcon = LoadIcon(NULL, MAKEINTRESOURCE(MB_ICONERROR));    //加载图标
+	WndClass.cbClsExtra = 0; // The default number of bytes of memory space after class
+	WndClass.cbWndExtra = 0; // The window instance defaults to extra bytes
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);     // White Background
+	WndClass.hCursor = LoadCursor(NULL, MAKEINTRESOURCE(IDC_ARROW));
+	WndClass.hIcon = LoadIcon(NULL, MAKEINTRESOURCE(MB_ICONERROR));
 	WndClass.hInstance = hInstance;
-	WndClass.lpfnWndProc = WindowProc; // 窗口回调函数
-	WndClass.lpszClassName = TEXT("XinhReadPad");                 //类名称 TEXT代表字符转换
-	WndClass.lpszMenuName = MAKEINTRESOURCE(IDR_MENU);            //菜单资源
-	WndClass.style = CS_HREDRAW | CS_VREDRAW;                     //风格
-	//注册窗口类
+	WndClass.lpfnWndProc = WindowProc; // WindowProc Function
+	WndClass.lpszClassName = TEXT("ReadPad");                     // TEXT represents character conversion
+	WndClass.lpszMenuName = MAKEINTRESOURCE(IDR_MENU);            
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;                     
+	
 	RegisterClass(&WndClass);
-	//创建窗口
-	HWND hWnd = CreateWindow(TEXT("XinhReadPad"),
-		TEXT("访问本"),
+	
+	HWND hWnd = CreateWindow(TEXT("ReadPad"),
+		TEXT("ReadPad"),
 		WS_OVERLAPPEDWINDOW,
 		500,
 		30,
@@ -47,20 +58,20 @@ int WINAPI WinMain(HINSTANCE hInstance,
 		NULL,
 		hInstance,
 		NULL);
-	//显示窗口
+	
 	ShowWindow(hWnd, SW_SHOW);
-	//更新窗口
+	
 	UpdateWindow(hWnd);
-	//消息循环
+	// Message Loop
 	MSG msg;
 	while (GetMessage(&msg,NULL,0,0))
 	{
-		TranslateMessage(&msg);  //将虚拟键消息转换为字符消息
-		DispatchMessage(&msg);   //将得到的消息分发给窗口程序
+		TranslateMessage(&msg);  // Convert virtual key messages to character messages
+		DispatchMessage(&msg);   // The message is distributed to the window procedure
 	}
 	return 0;
 }
-//回调函数 WindowProc
+// Callback function WindowProc()
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	static HWND hEditWnd;
@@ -70,18 +81,18 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_CREATE:
 	  hEditWnd = CreateWindow(TEXT("Edit"), NULL,
 			WS_CHILD | WS_VISIBLE | WS_VSCROLL | ES_MULTILINE | ES_WANTRETURN,
-			0,0,0,0,hWnd, (HMENU)IDC_EDIT, g_hInstance,NULL);//创建编辑控件
+			0,0,0,0,hWnd, (HMENU)IDC_EDIT, g_hInstance,NULL);// Create an edit control
 	  hFont = CreateFont(20, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE, GB2312_CHARSET,
 		  OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY,
-		  DEFAULT_PITCH | FF_SWISS, _T("楷体"));//创建字体
-	  //设置字体
+		  DEFAULT_PITCH | FF_SWISS, _T("妤蜂綋"));// Create Font
+	  
 	  SendMessage(hEditWnd, WM_SETFONT, (WPARAM)hFont, 0);
-	  //设置焦点
+	  
 	  SetFocus(hEditWnd);
 		break;
 	case WM_SIZE:
 		MoveWindow(hEditWnd, 0, 0, LOWORD(lParam),HIWORD(lParam),
-			FALSE);//移动窗口大小
+			FALSE);// Move Window size
 		break;
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
@@ -95,7 +106,7 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		{
 			OPENFILENAME ofn = { 0 };
 			ofn.lStructSize = sizeof(OPENFILENAME);
-			ofn.lpstrFilter = TEXT("文本文档(*.txt)\0*.txt\0所有文件(*.*)\0*.*\0\0");
+			ofn.lpstrFilter = TEXT("Text File(*.txt)\0*.txt\0All File(*.*)\0*.*\0\0");
 			ofn.lpstrFile = g_szFilePath;
 			ofn.nMaxFile = MAX_PATH;
 			ofn.Flags = OFN_FILEMUSTEXIST;
@@ -103,33 +114,33 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			{
 				return 0;
 			}
-			//MessageBox(NULL, g_szFilePath, TEXT("提示"), MB_OK);  //弹出得到路径
-			//读取文件
-			//得到文件句柄
+			//MessageBox(NULL, g_szFilePath, TEXT("鎻愮ず"), MB_OK);  //寮瑰嚭寰楀埌璺緞
+			// Read File
+			// Get File Handle
 			HANDLE hOpenFile = CreateFile(g_szFilePath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, NULL, NULL);
 
 			if (hOpenFile == INVALID_HANDLE_VALUE)
 			{
-				MessageBox(hWnd, _T("所选文件不能打开"),NULL,MB_OK);
+				MessageBox(hWnd, _T("The File can't open!"),NULL,MB_ICONERROR);
 				return 0;
 			}
-			//获取文件大小
+			// Get File size
 			DWORD dwFileSize;
 			DWORD dwReadSize;
 			dwFileSize = GetFileSize(hOpenFile, NULL);
-			//开辟内存
+			// Open up memory
 			TCHAR *pBuffer = (TCHAR*)malloc(dwFileSize + 1);
-			//读取文件
+			// Read file
 			ReadFile(hOpenFile, pBuffer, dwFileSize, &dwReadSize, NULL);
-			//清空缓存区
+			// Clear Buffer
 			pBuffer[dwFileSize] = '\0';
-			//将文本显示到控件
+			// Show Word to EditWnd
 			SetWindowText(hEditWnd, pBuffer);
-			//设置标题
+			// Set Title
 			_tcscpy_s(g_szFileTitle, sizeof(g_szFileTitle), g_szFilePath);
 			PathStripPath(g_szFileTitle);
 			SetWindowText(hWnd, g_szFilePath);
-			//释放内存
+			
 			free(pBuffer);
 		}
 			break;
@@ -138,12 +149,12 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		}
 		break;
 	case WM_CLOSE:
-		DestroyWindow(hWnd); //摧毁窗口
+		DestroyWindow(hWnd);
 		break;
 	case WM_DESTROY:
-		PostQuitMessage(0);  //退出进程
+		PostQuitMessage(0);
 		break;
 	}
-	return DefWindowProc(hWnd, uMsg, wParam, lParam); //调用窗口回调函数，交于系统处理
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
 	
 }
